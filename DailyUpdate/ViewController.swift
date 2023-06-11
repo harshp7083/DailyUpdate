17d3e7ad9988aca7896750f6adbb3f029aa174f2
jigmetdadul <jigmetdadul6@gmail.com>
2023-06-12 00:22:16 +0530

    Initial Commit

---
+++ DailyUpdate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyUpdate
+//
+//  Created by Jigmet stanzin Dadul on 12/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
